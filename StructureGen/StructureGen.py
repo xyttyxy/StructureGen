feba37b3a9e9b3558495d8acdb8216b680c5b014
Yantao Xia <xyttyxy@personalmicrosoftsoftware.ucla.edu>
2019-09-06 11:46:57 -0700

    update

--- StructureGen/StructureGen.py
+++ StructureGen/StructureGen.py
@@ -1,1 +1,16 @@
+from catkit.gen.surface import SlabGenerator
+from ase.build import bulk
+from ase.visualize import view
 
+atoms_bulk = bulk('Cu', 'fcc', cubic=True)
+gen = SlabGenerator(atoms_bulk,
+                    miller_index=(1,1,1),
+                    layers=4,
+                    fixed=2,
+                    vacuum=4,
+                    standardize_bulk=True,
+                    layer_type="trim")
+
+primitive = gen.get_slab()
+supercell = gen.get_slab(size=(3,4))
+view(supercell,viewer="RASMOL")
